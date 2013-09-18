ff9648e402ada6e3b2423c5a62accdb4bb43ce66
Katie Miller <katie.miller@legitscript.com>
2013-09-18 15:02:31 -0700

    Hook up delete action on frontend Move recipe parsing to a module to handle domains from bookmarklet

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,24 +1,6 @@
-require 'open-uri'
 class ListController < ApplicationController
+  include RecipeParser
   def index
-    url = params[:recipe_page_url]
-    doc = Nokogiri::HTML(open(url))
-
-    # For paleoplan.com
-  	raw_ingredients = doc.css("#ingredients ul").children.map { |li| li.text }
-  	@ingredients = parse_ingredients(raw_ingredients)
-  end
-
-  private
-
-  def parse_ingredients(raw_ingredients)
-  	raw_ingredients.map do |i|
-  		parsed = Ingreedy.parse i
-  		{ content: parsed.ingredient, quantity: parsed.amount, measurement: parsed.unit }
-		end
-  end
-
-  def relevant
-
+  	@ingredients = RecipeParser.parse_site(params[:recipe_page_url])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ActiveRecord::Base
 	belongs_to :list
 
-	validates :quantity, presence: true
 	validates :content, presence: true
 end
